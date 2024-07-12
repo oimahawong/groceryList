5bfce2a40329235e0dbf1775871f5883b9b6e4ef
Joshua Shor <44251534+JoshShor@users.noreply.github.com>
2024-07-12 00:18:12 -0700

    works on my end now, still need to resolve error messages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import GroceryList from './components/GroceryList';
@@ -5,16 +6,29 @@ import GroceryEST from './components/GroceryEST';
 import ListGroup from './components/ListGroup';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/Home" element={<Home />} />
-                <Route path="/grocery-list" element={<GroceryList />} />
-                <Route path="/grocery-est" element={<GroceryEST />} />
-                <Route path="/list-group" element={<ListGroup />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/grocery-list" element={<GroceryList />} />
+        <Route path="/grocery-est" element={<GroceryEST />} />
+        <Route path="/list-group" element={<ListGroup />} />
+        
+        {/* Catch-all route for unmatched paths */}
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
+  );
 }
+
+// NoMatch component to handle unmatched routes
+const NoMatch = () => {
+  return (
+    <div>
+      <h1>404 - Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
 
 export default App;
